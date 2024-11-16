e7fd8bf872c78366fb9b370701728e42afc12216
Victor Cruz <victorcruzs016@gmail.com>
2024-11-16 14:23:51 -0400

    Subir Proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgFor, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'GaleriaDeImagenes';
+  title = 'Galeria De Imagenes';
+
+  imagenes: string[] = [];
+
+  constructor() {
+    this.cargarImagenes();
+  }
+
+  cargarImagenes() {
+    const totalImagenes = 12;
+    for (let i = 1; i <= totalImagenes; i++) {
+      this.imagenes.push(`assets/imgs/img${i}.jpg`);
+    }
+  }
 }
+
+
